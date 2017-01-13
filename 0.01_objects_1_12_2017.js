d77d9bd960c7603f96b9d0a93d37d1f19b486709
bristanley <brian.stanley89@outlook.com>
2017-01-12 22:10:15 -0500

    Objects from JS TGP

---
+++ 0.01_objects_1_12_2017.js
@@ -0,0 +1,24 @@
+var myLibrary = {
+	theAlchemist: {
+		author    		: "Paulo Coehlo",
+		length    		: 200,
+		yearPublished	: 1988,
+		genre			: "Fiction"
+	},
+	gameOfThrones: {
+		author			: "George R.R. Martin",
+		length			: 694,
+		yearPublished	: 1996,
+		genre			: "Fantasy"
+	},
+	endersGame: {
+		author			: "Orson Scott Card",
+		length			: 324,
+		yearPublished	:1985
+		genre			: "Sci-Fi"
+	}
+}
+
+console.log(myLibrary.theAlchemist.author);
+console.log(myLibrary.endersGame.yearPublished);
+
