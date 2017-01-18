fac60b2a9a265ddf834014ecc6f21404076baa17
bristanley <brian.stanley89@outlook.com>
2017-01-17 22:21:30 -0500

    Numbers in JS Math.

---
+++ 0.04_numbers.js
@@ -0,0 +1,18 @@
+var myVariable = 4;
+
+var a = "5";
+var b = 5;
+
+console.log(a + b);
+//console.log(typeof (a + b) );
+
+console.log(Math.round(7.5));
+
+console.log(Math.floor(7.8));
+
+console.log(Math.ceil(7.2));
+
+console.log(Math.max(7, 4, 9, 8));
+
+console.log(Math.PI);
+
