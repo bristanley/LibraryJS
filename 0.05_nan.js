4517f8eb2b68bf6d9b62fcd7b0e498c52f252727
bristanley <brian.stanley89@outlook.com>
2017-01-18 21:19:11 -0500

    Nan

---
+++ 0.05_nan.js
@@ -0,0 +1,24 @@
+var a = "apple";
+var b =5;
+
+// NaN = not a number
+
+//console.log(a * b);
+
+
+/*if (isNaN(a)) {
+
+	console.log("That ain't no number!");
+	var a = 6;
+} else {
+
+	console.log("meaning of life is " + (a + b));
+}*/
+
+//double negative with ! addition
+
+if (!isNaN(a)) {
+
+	console.log("meaning of life is " + (a + b));
+	
+}
