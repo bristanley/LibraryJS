61ee398f7df1ab66db7429e1270ace09f1421769
bristanley <brian.stanley89@outlook.com>
2017-01-22 20:00:25 -0500

    Slice and split strings

---
+++ 0.08_slice-and-split-strings.js
@@ -0,0 +1,19 @@
+var str ="Hello World";
+
+var str2 = str.slice(2, 9);
+
+
+console.log(str2);
+
+var str3 = str.slice(2);
+
+console.log(str3);
+
+var tags = "meat, ham, salami, pork, beef"
+
+console.log(tags);
+
+var tagsArray = tags.split(",");
+
+console.log(tagsArray);
+
