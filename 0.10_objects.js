6bff9579bf13addad7f4e7ecd27f6d0e2fc135d2
bristanley <brian.stanley89@outlook.com>
2017-01-24 21:33:29 -0500

    objects

---
+++ 0.10_objects.js
@@ -0,0 +1,16 @@
+//var myCar = newCar()
+
+var myString = new String()
+
+myString = "hello";
+
+console.log(myString);
+
+console.log(myString.length);
+
+console.log(myString.toLowerCase());
+
+var myString2 = "hi there";
+
+console.log(myString2);
+
