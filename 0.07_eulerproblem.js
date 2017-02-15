f59369898f6be60fe19bb1c3555184141d3e39a7
Brian Stanley <Brian Stanley>
2017-02-14 22:12:59 -0500

    templat strings still not working. Object lits make sense

---
+++ 0.07_eulerproblem.js
@@ -0,0 +1,6 @@
+for (var i = 0; i < 1000; i++){
+	if (i / 5 && i / 3){
+		console.log(i);
+	}
+		
+}
---
+++ 0.25_ESsix_object_literals.js
@@ -0,0 +1,21 @@
+function object() {
+
+	var name = "bob";
+
+	var hair = "black";
+
+	var man = {
+		name: name,
+		hair: hair,
+		ran(x){
+			console.log('he walked ${x} miles');
+		}
+	};
+
+	// console.log(man.name);
+	// console.log(man.hair);
+
+	console.log(man.ran(500));
+};
+
+object();
